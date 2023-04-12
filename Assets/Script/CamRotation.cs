bfa580765c10b102ff96dd98d393193e50a6eb35
ayushya rao <ayushya19@gmail.com>
2023-04-12 17:58:29 +0530

    Camera view motion added

---
+++ Assets/Script/CamRotation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+public class CamRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+     
+ 
+
+    private float x;
+    private float y;
+    private Vector3 rotateValue;
+
+    void Update()
+    {
+        y = Input.GetAxis("Mouse X");
+        x = Input.GetAxis("Mouse Y");
+        Debug.Log(x + ":" + y);
+        rotateValue = new Vector3(x, y * -1, 0);
+        transform.eulerAngles = transform.eulerAngles - rotateValue;
+    }
+
+}
---
+++ Library/Bee/artifacts/emscripten/ngyxr/wrapper_emcc.bat
@@ -0,0 +1,4 @@
+
+@echo off
+"D:\Software\Unity\2021.3.6f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emcc.bat" %* < nul
+exit %ERRORLEVEL%
